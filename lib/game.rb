e64fe6bf4fb5ae12991378a88d12909cc5fd3883
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-07-06 13:04:52 +0200

    Add get player choice function

--- lib/game.rb
+++ lib/game.rb
@@ -7,4 +7,38 @@ class Game
     @player1 = Player.new("x")
     @player2 = Player.new("o")
   end
+
+  def run_game
+    while true
+      @gameboard.draw_board
+      if check_full_board
+        end_game("full")
+      end
+
+    end
+  end
+
+  private
+
+  def end_game(param)
+    if param == "full"
+      puts "Game is over. Board is full"
+    end
+  end
+
+  def check_victory
+
+  end
+
+  def get_player_choice(player)
+
+  end
+
+  def check_full_board
+    @gameboard.each do |row|
+      if row.any?(' ')
+        return true
+      end
+    end
+  end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,25 @@
 class Player
   attr_accessor :symbol
 
-  def initialize(symbol, name)
+  def initialize(symbol)
     @symbol = symbol
+  end
+
+  def get_player_choice
+    begin
+      puts "Please enter a number between 1 and 9:"
+      input = gets.chomp
+
+       # Check if the input is a valid integer
+      if input.match?(/^\d+$/)
+        number = input.to_i
+      else
+        number = nil
+      end
+
+    end while number.nil? || number < 1 || number > 9
+
+    number
+
   end
 end
